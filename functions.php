e5476b250db242167bc1d54ae642733fa11b589c
laura <laura.brager@edu.univ-fcomte.fr>
2021-03-04 16:08:05 +0100

    commit
    theme enfant

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+** activation theme
+**/
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+// enqueue child styles
+wp_enqueue_style('child-style', get_stylesheet_directory_uri() .'/style.css', array('parent-style'));
+}
+?>
